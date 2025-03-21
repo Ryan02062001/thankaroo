67bae1a828bc904c92aada6c1ea54e2b51271912
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2025-03-21 17:06:09 -0500

    Update to feature section

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -12,7 +12,7 @@ export default function FeaturesSection() {
               Features
             </div>
             <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-            Why Thankaroo Helps You Track Gifts
+            How Thankaroo Helps You Track Gifts
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
             Thankaroo simplifies the process of tracking gifts and sending thank-you notes.
