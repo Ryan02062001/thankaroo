87fcdf500a279bda8764d4dd900b067583dbfa01
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2025-03-22 11:19:05 -0500

    Changed header height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-[#FFC5B2] bg-[#fefefe] shadow-sm">
-      <div className="max-w-7xl mx-auto flex h-16 items-center px-4">
+      <div className="max-w-7xl mx-auto flex h-20 items-center px-4">
         <div className="flex items-center gap-2">
           <Link className="flex items-center gap-2"href="/">
           <Heart className="h-6 w-6 text-[#A4D9C9]" />
