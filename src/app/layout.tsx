7cf62c5471e5701191020a8a666e4cab7b2d6565
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2025-09-03 13:30:25 -0500

    FBpixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,24 @@ gtag('js', new Date());
 
 gtag('config', 'AW-10967263816');`}
         </Script>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1936098137232512');
+fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body className={`${poppins.variable} ${geistMono.variable} antialiased font-sans`}>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1936098137232512&ev=PageView&noscript=1" alt="" />
+        </noscript>
         <Header />
         <main id="main-content" role="main">
           <ReminderProvider>
