a8d412798a5a54eabcdce332f23b8837e0a0d693
Ryan Cooper <ryancooper@Ryans-MacBook-Pro.local>
2025-09-03 13:13:47 -0500

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,17 +26,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1936098137232512');
-fbq('track', 'PageView');`}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-10967263816" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-10967263816');`}
         </Script>
       </head>
       <body className={`${poppins.variable} ${geistMono.variable} antialiased font-sans`}>
